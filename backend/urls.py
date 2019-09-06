7967d4348602ec6e0e3d00aa24d37b7e266f5d27
Josh Teneycke <josh.teneycke@gmail.com>
2019-09-06 11:54:47 -0400

    Configure proxying for localhost and production

--- backend/urls.py
+++ backend/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # ... the rest of the urlpatterns ...
+    # must be catch-all for pushState to work
+    url(r'^', views.FrontendAppView.as_view()),
+    path('admin/', admin.site.urls)
 ]
