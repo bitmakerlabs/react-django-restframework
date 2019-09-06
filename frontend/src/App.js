8a3b6a3e482ee132fd2e359d70fc285ea63a0c52
Josh Teneycke <josh.teneycke@gmail.com>
2019-09-06 14:39:17 -0400

    Make react consume our example API route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [musicians, setMusicians] = useState([])
+
+  useEffect(()=> {
+
+    axios.get('/api/musicians.json')
+      .then((resp) => {
+        console.log("Api Response", resp.data)
+        setMusicians(resp.data)
+      })
+
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {
+            musicians.map((musician, index) => {
+              return <li key={index}>{musician.first_name} {musician.last_name}</li>
+            })
+          }
+
+        </ul>
       </header>
     </div>
   );
